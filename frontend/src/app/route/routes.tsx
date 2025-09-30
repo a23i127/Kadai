08fd34317283a8bee1e3960d7ed3f70ae66d9ed7
a23i127 <a23i127ne@ous.jp>
2025-09-30 20:32:47 +0900

    フロントルーティング,環境作った

---
+++ frontend/src/app/route/routes.tsx
@@ -0,0 +1,13 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "../App";
+
+const AppRoutes = () => (
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      {/* 追加ページ例: <Route path="/repos/:owner/:repo/*" element={<DisplayArea />} /> */}
+    </Routes>
+  </BrowserRouter>
+);
+
+export default AppRoutes;
